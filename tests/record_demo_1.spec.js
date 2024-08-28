cf30b4f4b9a27c8ef45c844c97d2b2f4d97525ea
Elsie <ojemba@Ojembas-MacBook-Pro.local>
2024-08-28 15:56:09 +0200

    how to record a test

---
+++ tests/record_demo_1.spec.js
@@ -0,0 +1,20 @@
+import {test, expect} from '@playwright/test'
+const { chromium } = require('@playwright/test')
+
+test('record demo 1', async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').dblclick();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+});
